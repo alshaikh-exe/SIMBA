d80d645ae405e2e0c98dfe9c82abb5d5c9efd5c8
Abdulla Alshaikh <alshaikhabdulla6@gmail.com>
2025-08-27 13:21:31 +0300

    patched up Cart component & cart page

--- src/components/Booking/Cart/Cart.jsx
+++ src/components/Booking/Cart/Cart.jsx
@@ -1,11 +1,9 @@
 // src/pages/Booking/CartPage/CartPage.jsx
 import React from "react";
-import Cart from "../../../components/Booking/Cart/Cart";
 
-export default function CartPage({ user, onCartUpdate }) {
+export default function Cart({ user, onCartUpdate }) {
   return (
     <div style={{ padding: "1rem" }}>
-      <Cart user={user} onCartUpdate={onCartUpdate} />
     </div>
   );
 }
